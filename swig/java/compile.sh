652c1bc9322228b20f4b6757f12491ffc1fe9b21
K Yamaguchi <quinita@gmail.com>
2013-12-27 13:20:26 +0900

    add comment

--- swig/java/compile.sh
+++ swig/java/compile.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 #http://www.swig.org/Doc2.0/SWIGDocumentation.html
 
-#1. create _wrap.cxx and wrapper source files by swig.
 [ -d src ] || mkdir src
 [ -d classes ] || mkdir classes
+
+#1. create _wrap.cxx and wrapper source files by swig (you can change the package name)
 swig -c++ -java -I../../src/  -o normalize_numexp_wrap.cxx -package jp.ac.tohoku.ecei.cl.numexp  -outdir src ../normalize_numexp.i
 
-#2. compile _wrap.cxx (JNI header files are necessary.)
+#2. compile _wrap.cxx (JNI header files are necessary)
 gcc -O2 -fPIC -c normalize_numexp_wrap.cxx -I../../src/ -I/usr/lib/jvm/java-7-oracle/include -I/usr/lib/jvm/java-7-oracle/include/linux
 
 #3. create shared library
