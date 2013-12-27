36a6ba68214b7db567bafc3b9afb241a214230f7
K Yamaguchi <quinita@gmail.com>
2013-12-27 11:14:21 +0900

    modify binding test script

--- swig/python/test_normalize_numexp.py
+++ swig/python/test_normalize_numexp.py
@@ -1,13 +1,15 @@
 #coding:utf-8
 import sys
-sys.path.append("/home/katsuma/local/python/lib/python2.7/site-packages/")
 from normalize_numexp import *
 
 n = NormalizeNumexp("ja")
-text = "魔女狩りは15世紀〜18世紀にかけてみられ、全ヨーロッパで4万人が処刑された"
+#text = "魔女狩りは15世紀〜18世紀にかけてみられ、全ヨーロッパで4万人が処刑された"
 result = StringVector(0)
-n.normalize(text, result)
 
-print "text:",text
-for r in result :
-    print r
+for text in sys.stdin:
+    text = text.rstrip('\n')
+    n.normalize(text, result)
+    print "text:",text
+    for r in result :
+        print r
+    print
